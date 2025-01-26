ff3553c044975e4bf965804cfb8dd00fa00cb0e5
João <mymailisthis@gmail.com>
2025-01-26 09:20:32 +0000

    mmx farm info, changed

--- mmx-daily-overview/mmx-daily-overview.js
+++ mmx-daily-overview/mmx-daily-overview.js
@@ -152,7 +152,7 @@ async function getNetSpace() {
                 reject(err);
             } else {
                 const result = stdout.split("\n");
-                resolve(result[0]);
+                resolve(result[2]);
             }
         });
     });
@@ -167,7 +167,7 @@ async function getFarmSpace() {
                 reject(err);
             } else {
                 const result = stdout.split("\n");
-                let total = result[3].split(" ");
+                let total = result[5].split(" ");
                 resolve(total[2] * 1000000000000);
             }
         });
